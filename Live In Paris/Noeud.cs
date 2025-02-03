223479a74c3e588c4544e075d174515525a23647
la1chevre <gregoire.legros@edu.devinci.fr>
2025-02-03 10:39:56 +0100

    ajout class
    kilian est beau

---
+++ Live In Paris/Noeud.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Live_In_Paris
+{
+    internal class Noeud
+    {
+        int nombre;
+    }
+}
