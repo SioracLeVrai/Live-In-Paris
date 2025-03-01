817f8ad7db08b1210ab3f15dee5eb95e57a6b0cf
SioracLeVrai <103414597+SioracLeVrai@users.noreply.github.com>
2025-03-01 18:54:00 +0100

    Update Program.cs

--- Live In Paris/Program.cs
+++ Live In Paris/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using SkiaSharp;
+using System;
 
 
 namespace Live_In_Paris
@@ -8,9 +9,20 @@ namespace Live_In_Paris
         static void Main(string[] args)
         {
             Graphe graphe = new Graphe();
-            graphe.AjouterNoeud("Paris");
-            graphe.AjouterNoeud("Lyon");
-            graphe.AjouterLien("Paris", "Lyon", 450);
+            graphe.CreationGraph();
+            Console.WriteLine("Les liens caractérisant le graph sont : ");
+            graphe.AffichageLiens();
+
+            Console.WriteLine("\nLa matrice du graph est : ");
+            graphe.MatriceAdjacence();
+
+            Console.WriteLine("\nParcour en largeur : " + graphe.ParcoursEnLargeur("0"));
+            Console.WriteLine("\nParcour en profondeur : " + graphe.ParcoursEnProfondeur("0"));
+            Console.WriteLine("\nLe graphe est connexe : " + graphe.Connexe());
+            Console.WriteLine("\nUn cycle est : " + graphe.Cycle(graphe.Noeuds[0]) + ". Il existe donc un cycle.");
+
+
+            graphe.DessinerGraphe();
         }
     }
 }
